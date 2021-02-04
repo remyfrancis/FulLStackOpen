c5067618c6bdb8e8cac361034efe707c0c3a669b
Remy <remy.e.francis@gmail.com>
2021-02-04 07:40:09 -0400

    Exercise 1.3 incomplete

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,20 +3,30 @@ import ReactDOM from 'react-dom';
 
 //FSO Exercises 1.1 and 1.2
 
-const App = (props) => {
+const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  
+
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content part1={part1.name} part2={part2.name} part3={part3.name} exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
     </div>
   )
 }
@@ -30,11 +40,12 @@ const Header = (props) => {
 }
 
 const Part = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <p>{props.part1.name} {props.part1.exercises}</p>
+      <p>{props.part2.name} {props.part2.exercises}</p>
+      <p>{props.part3.name} {props.part3.exercises}</p>
     </div>
   )
 }
@@ -42,19 +53,17 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part1={props.part1} exercises1={props.exercises1}/>
-      <Part part2={props.part2} exercises2={props.exercises2}/>
-      <Part part3={props.part3} exercises3={props.exercises3}/>
+      <Part part1={props.part1.name} exercises1={props.part1.exercises}/>
+      <Part part2={props.part2.name} exercises2={props.part2.exercises}/>
+      <Part part3={props.part3.name} exercises3={props.part3.exercises}/>
     </div>
   )
 }
 
 const Total = (props) => {
-  console.log(props.exercises1 + props.exercises2)
-  console.log(props.exercises3)
   return (
     <div>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
     </div>
   )
 }
